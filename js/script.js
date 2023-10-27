0df349228abed6424900554179a09c141b82a0fc
Anthony64M <jymal6anthony4@gmail.com>
2023-10-27 13:19:29 +0300

    chatbot

--- js/script.js
+++ js/script.js
@@ -1,122 +1,166 @@
-jQuery(document).ready(function($) {
-    // Chat interface code
-    jQuery(document).on('click', '.iconInner', function(e) {
-        jQuery(this).parents('.botIcon').addClass('showBotSubject');
-        $("[name='msg']").focus();
-    });
+// Imports
+import { SessionsClient } from "https://esm.sh/dialogflow";
 
-    jQuery(document).on('click', '.closeBtn, .chat_close_icon', function(e) {
-        jQuery(this).parents('.botIcon').removeClass('showBotSubject');
-        jQuery(this).parents('.botIcon').removeClass('showMessenger');
-    });
 
-    jQuery(document).on('submit', '#botSubject', function(e) {
-        e.preventDefault();
+$(document).ready(function () {
+  // Function to show the chatbot subject
+  function showChatBotSubject() {
+    $(this).parents('.botIcon').addClass('showBotSubject');
+    $("[name='msg']").focus();
+  }
 
-        jQuery(this).parents('.botIcon').removeClass('showBotSubject');
-        jQuery(this).parents('.botIcon').addClass('showMessenger');
-    });
+  // Function to close the chatbot
+  function closeChatBot() {
+    var botIcon = $(this).parents('.botIcon');
+    botIcon.removeClass('showBotSubject');
+    botIcon.removeClass('showMessenger');
+  }
 
-    $(document).on("submit", "#messenger", function(e) {
-        e.preventDefault();
+  // Function to handle the bot subject form submission
+  function handleBotSubjectFormSubmit(e) {
+    e.preventDefault();
 
-        var val = $("[name=msg]").val().toLowerCase();
-        var mainval = $("[name=msg]").val();
-        var name = '';
-        var nowtime = new Date();
-        var nowhour = nowtime.getHours();
+    var botIcon = $(this).parents('.botIcon');
+    botIcon.removeClass('showBotSubject');
+    botIcon.addClass('showMessenger');
+  }
 
-        function userMsg(msg) {
-            $('.Messages_list').append('<div class="msg user"><span class="avtr"><figure style="background-image: url(https://mrseankumar25.github.io/Sandeep-Kumar-Frontend-Developer-UI-Specialist/images/avatar.png)"></figure></span><span class="responsText">' + mainval + '</span></div>');
-        }
+  // Event listeners for chatbot subject and close buttons
+  $(document).on('click', '.iconInner', showChatBotSubject);
+  $(document).on('click', '.closeBtn, .chat_close_icon', closeChatBot);
 
-        function appendMsg(msg) {
-            $('.Messages_list').append('<div class="msg"><span class="avtr"><figure style="background-image: url(https://mrseankumar25.github.io/Sandeep-Kumar-Frontend-Developer-UI-Specialist/images/avatar.png)"></figure></span><span class="responsText">' + msg + '</span></div>');
-            $("[name='msg']").val("");
-        }
+  // Event listener for bot subject form submission
+  $(document).on('submit', '#botSubject', handleBotSubjectFormSubmit);
 
-        userMsg(mainval);
+  // Chatbot Code
+  $(document).on("submit", "#messenger", function (e) {
+    e.preventDefault();
 
-        // Handle other cases here
-        // ...
+    var val = $("[name=msg]").val().toLowerCase();
+    var mainval = $("[name=msg]").val();
+    var name = '';
+    var nowtime = new Date();
+    var nowhour = nowtime.getHours();
 
-        var lastMsg = $('.Messages_list').find('.msg').last().offset().top;
-        $('.Messages').animate({ scrollTop: lastMsg }, 'slow');
-    });
-
-    // Chatbot code
-    try {
-        // Bot responses
-        const botResponses = {
-            greeting: ['Hi there! Welcome to Beisa Hotel.', 'Hello! How may I help you?'],
-            rooms: ['We have standard rooms available. Deluxe rooms with extra amenities are also available.'],
-            booking: ['Please visit our website www.beisahotelandconferencecentre.co.ke to see room availability and book online.'],
-            contact: ['Feel free to call us at +254 722 454978 for any questions or to book over the phone.']
-        };
-
-        // Validate bot responses
-        if (!botResponses || Object.keys(botResponses).length === 0) {
-            throw new Error('No bot responses defined!');
-        }
-
-        // Function to create the chatbot
-        const createChatbot = (botId) => {
-            // Chatbot UI and elements - Note: You need to define chatbotUI
-            const chatEl = document.createElement('div');
-            chatEl.innerHTML = chatbotUI; // Make sure chatbotUI is defined
-            const messagesEl = chatEl.querySelector('.messages');
-            const formEl = chatEl.querySelector('#messenger');
-            const inputEl = formEl.querySelector('input');
-
-            // Bot response handler - You need to handle bot responses in this function
-            const botResponse = (text) => {
-                // Handle bot responses, e.g., by appending messages to the chat interface
-                appendMsg(text);
-            };
-
-            // Form submit handler
-            formEl.addEventListener('submit', (e) => {
-                e.preventDefault();
-
-                const msg = inputEl.value;
-                inputEl.value = '';
-
-                // Handle user input and generate bot responses
-                let response = 'Sorry, I did not understand that.';
-
-                if (msg.includes('hello')) {
-                    response = botResponses.greeting[0];
-                } else if (msg.includes('room')) {
-                    response = botResponses.rooms[0]; // Get the first response
-                } else if (msg.includes('book')) {
-                    response = botResponses.booking[0]; // Get the first response
-                } else if (msg.includes('contact')) {
-                    response = botResponses.contact[0]; // Get the first response
-                }
-
-                // Display the bot's response in the chat interface
-                appendMsg(response);
-            });
-        };
-
-        // Function to submit a message to WhatsApp
-        const submitToWhatsApp = async (botId, userMsg, botMsg) => {
-            try {
-                // API call to send a WhatsApp message - Replace instanceXXXX and tokenYYYY with actual values
-                await fetch('https://api.chat-api.com/instanceXXXX/message?token=YYYY', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        phone: '+254722454978',
-                        body: `Bot #${botId} transcript: \nUser: ${userMsg}\nBot: ${botMsg}`
-                    })
-                });
-            } catch (err) {
-                console.error('Error submitting to WhatsApp', err);
-            }
-        };
-    } catch (err) {
-        // Handle errors
-        console.error(err);
-        botResponse('Sorry, something went wrong. Please try again later.'); // You need to define botResponse
+    function userMsg(msg) {
+      $('.Messages_list').append('<div class="msg user"><span class="avtr"><figure style="background-image: url(https://mrseankumar25.github.io/Sandeep-Kumar-Frontend-Developer-UI-Specialist/images/avatar.png)"></figure></span><span class="responsText">' + mainval + '</span></div>');
     }
+
+    function appendMsg(msg) {
+      $('.Messages_list').append('<div class="msg"><span class="avtr"><figure style="background-image: url(https://mrseankumar25.github.io/Sandeep-Kumar-Frontend-Developer-UI-Specialist/images/avatar.png)"></figure></span><span class="responsText">' + msg + '</span></div>');
+      $("[name='msg']").val("");
+    }
+
+    userMsg(mainval);
+
+    if (val.indexOf("hello") > -1 || val.indexOf("hi") > -1 || val.indexOf("good morning") > -1 || val.indexOf("good afternoon") > -1 || val.indexOf("good evening") > -1 || val.indexOf("good night") > -1) {
+      if (nowhour >= 12 && nowhour <= 16) {
+        appendMsg('Good afternoon from Beisa Hotel!');
+      } else if (nowhour >= 10 && nowhour <= 12) {
+        appendMsg('Hi there! How can Beisa Hotel assist you?');
+      } else if (nowhour >= 0 && nowhour <= 10) {
+        appendMsg('Good morning! How can Beisa Hotel assist you today?');
+      } else {
+        appendMsg('Good evening from Beisa Hotel!');
+      }
+
+      appendMsg("May I know your name?");
+    } else if (val.indexOf("i have a problem with") > -1) {
+      if (val.indexOf("reservation") > -1 || val.indexOf("booking") > -1) {
+        appendMsg("I can help you with your reservation. Please provide your reservation details.");
+      } else {
+        appendMsg("I'm sorry, I couldn't understand your request. Please ask something else related to Beisa Hotel.");
+      }
+    } else if (val.indexOf("my name is ") > -1 || val.indexOf("i am ") > -1 || val.indexOf("i'm ") > -1 || val.split(" ").length < 2) {
+      if (val.indexOf("my name is") > -1) {
+        name = val.replace("my name is", "");
+      } else if (val.indexOf("i am") > -1) {
+        name = val.replace("i am", "");
+      } else if (val.indexOf("i'm") > -1) {
+        name = val.replace("i'm", "");
+      } else {
+        name = mainval;
+      }
+
+      appendMsg("Hello, " + name + "! How can Beisa Hotel assist you today?");
+    } else {
+      appendMsg("I'm sorry, I couldn't understand your request. Please ask something related to Beisa Hotel.");
+    }
+
+    var lastMsg = $('.Messages_list').find('.msg').last().offset().top;
+    $('.Messages').animate({
+      scrollTop: lastMsg
+    }, 'slow');
+  });
 });
+
+// Dialogflow setup
+const sessionClient = new SessionsClient({ projectId: 'beisa-403309' });
+
+// Configuration
+const BOT_NAME = 'Claude';
+const BOT_AVATAR = 'bot.png';
+const USER_AVATAR = 'user.png';
+
+// DOM Elements
+const chatbot = document.getElementById('chatbot');
+const chatHistory = document.getElementById('chat-history');
+const chatForm = document.getElementById('messenger');
+const chatInput = document.getElementById('chat-input');
+
+// Initialize chatbot
+function init() {
+  // Show greeting
+  showBotMessage("Hi there! I'm Claude.");
+
+  // Form submit listener
+  chatForm.addEventListener('submit', onChatSubmit);
+}
+
+// Form submit handler
+function onChatSubmit(e) {
+  e.preventDefault();
+
+  let input = chatInput.value;
+  showUserMessage(input);
+  getBotResponse(input);
+  chatInput.value = '';
+}
+
+// Get response from Dialogflow
+async function getBotResponse(input) {
+  let request = {
+    session: sessionClient.sessionPath('beisa-403309', 'unique-session-id'),
+    queryInput: {
+      text: {
+        text: input,
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  let responses = await sessionClient.detectIntent(request);
+  let botMessage = responses[0].queryResult.fulfillmentText;
+  showBotMessage(botMessage);
+}
+
+// Show user message
+function showUserMessage(msg) {
+  chatHistory.innerHTML += `
+    <div class="user-msg">
+      <span>${msg}</span>
+    </div>
+  `;
+}
+
+// Show bot response
+function showBotMessage(msg) {
+  chatHistory.innerHTML += `
+    <div class="bot-msg">
+      <span>${msg}</span>
+    </div>
+  `;
+}
+
+// Initialize
+init();
