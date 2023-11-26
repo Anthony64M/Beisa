336c2d8ed8301d98e1fffe518fe8371956604236
Anthony64M <jymal6anthony4@gmail.com>
2023-11-26 18:18:17 +0300

    generate response on all js

--- js/beis.js
+++ js/beis.js
@@ -1,122 +1,73 @@
-// 3d animation
-const angle = 20;
-const rotateCard = window;
-
-const lerp = (start, end, amount) => {
-	return (1 - amount) * start + amount * end;
-};
-
-const remap = (value, oldMax, newMax) => {
-	const newValue = ((value + oldMax) * (newMax * 2)) / (oldMax * 2) - newMax;
-	return Math.min(Math.max(newValue, -newMax), newMax);
-};
-
-window.addEventListener("DOMContentLoaded", (event) => {
-	const cards = document.querySelectorAll(".card");
-	cards.forEach((e) => {		
-		e.addEventListener("mousemove", (event) => {
-			const rect = e.getBoundingClientRect();
-			const centerX = (rect.left + rect.right) / 2;
-			const centerY = (rect.top + rect.bottom) / 2;
-			const posX = event.pageX - centerX;
-			const posY = event.pageY - centerY;
-			const x = remap(posX, rect.width / 2, angle);
-			const y = remap(posY, rect.height / 2, angle);
-			e.dataset.rotateX = x;
-			e.dataset.rotateY = -y;
-		});
-		
-		e.addEventListener("mouseout", (event) => {
-			e.dataset.rotateX = 0;
-			e.dataset.rotateY = 0;
-		});
-	});
-	
-	const update = () => {
-		cards.forEach((e) => {
-			let currentX = parseFloat(e.style.getPropertyValue('--rotateY').slice(0, -1));
-			let currentY = parseFloat(e.style.getPropertyValue('--rotateX').slice(0, -1));
-			if (isNaN(currentX)) currentX = 0;
-			if (isNaN(currentY)) currentY = 0;
-			const x = lerp(currentX, e.dataset.rotateX, 0.05);
-			const y = lerp(currentY, e.dataset.rotateY, 0.05);
-			e.style.setProperty("--rotateY", x + "deg");
-			e.style.setProperty("--rotateX", y + "deg");
-		})
-	}
-	setInterval (update,1000/60)
-});
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage = null; // Variable to store user's message
-const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
+const API_KEY = ""; // Leave API key empty
 const inputInitHeight = chatInput.scrollHeight;
+
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
+  // Create a chat <li> element with passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
 }
+
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = chatElement.querySelector("p");
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
+    let response = "I'm sorry, I don't understand. Please contact customer care at +254 722 454978. for assistance.";
+  
+    if (userMessage.toLowerCase().includes("Hello", "hello", "Hi", "hi")) {
+      response = "Welcome to Beisa Hotel. If you looking for  room reservations type room reservation ,if you want conference details type conference.Thankyou.";
+    } else if (userMessage.toLowerCase().includes("hotel")) {
+      response = "To make a room reservation at Beisa Hotel, please visit our website  at the book now section or speed connection  contact us at +254 722 454978.";
+    } else if (userMessage.toLowerCase().includes("conference")) {
+      response = "Beisa Hotel offers various amenities including a pool, restuarant and conference center head to the gallery section to view our conference centre then contact +254 722 454978. to get the variance in each conference   . For more details, please contact us at +254 722 454978.";
     }
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
-const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    // Add more conditions and responses as needed
+  
+    messageElement.textContent = response;
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
+  }
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  if(!userMessage) return;
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+  
+  setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
+    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 600);
 }
+
 chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
+  // Adjust the height of the input textarea based on its content
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
+
 chatInput.addEventListener("keydown", (e) => {
-    // If Enter key is pressed without Shift key and the window 
-    // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat();
-    }
+  // If Enter key is pressed without Shift key and the window 
+  // width is greater than 800px, handle the chat
+  if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat();
+  }
 });
+
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
